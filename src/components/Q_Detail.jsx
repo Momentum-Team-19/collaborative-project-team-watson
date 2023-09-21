f7169d656f1ddfb7ffa27efda5de336a8bf60da7
Nathan Vogt <merrick.vogt@gmail.com>
2023-09-21 17:11:09 -0400

    edit component now renders and functional. uses patch request to update the question on the backend. the page needs to re-render to update the information. interestingly, the updated question moves to the bottom of the query.

--- src/components/Q_Detail.jsx
+++ src/components/Q_Detail.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import { Typography, Box, Stack, Container, Button } from '@mui/material';
-import axios from 'axios';
-import Q_Delete from './Q_Delete';
+import { Typography, Box, Stack, Container, Button } from "@mui/material";
+import Q_Delete from "./Q_Delete";
+import Q_Edit from "./Q_Edit";
+
 
 const Q_Detail = ({ selectedQuestion, token, setSelectedQuestion }) => {
   if (!selectedQuestion) {
@@ -16,29 +16,36 @@ const Q_Detail = ({ selectedQuestion, token, setSelectedQuestion }) => {
     <>
       <Box textAlign="left">
         <Typography variant="h4">{selectedQuestion.title}</Typography>
-        <Typography variant="h5">Author: {selectedQuestion.author || 'N/A'}</Typography>
-        <Stack direction="row" spacing={2}>
-          {/* <Typography variant="body2">Date Created: {selectedQuestion.date}</Typography> */}
-          {/* <Typography variant="body2">Project: {selectedQuestion.project.name || 'N/A'}</Typography> */}
-          {/* <Typography variant="body2">Tags: {selectedQuestion.tags.join(', ')}</Typography>
-          <Typography variant="body2">Votes: {selectedQuestion.votes}</Typography> */}
-        </Stack>
+        <Typography variant="h5">
+          Author: {selectedQuestion.author || "N/A"}
+        </Typography>
       </Box>
-      <Container maxWidth={false} style={{ width: '100%', backgroundColor: 'lightgrey' }}>
+
+      <Container
+        maxWidth={false}
+        style={{ width: "100%", backgroundColor: "lightgrey" }}
+      >
         <Box textAlign="left">
           <Typography variant="body2">Body: {selectedQuestion.body}</Typography>
         </Box>
       </Container>
-      {/* <Typography variant="body1">{selectedQuestion.answers[0]}</Typography> */}
+
       <Typography variant="body2">Write an Answer</Typography>
-      {/* Back button */}
-      <Button 
-        variant="contained" 
-        style={{ backgroundColor: 'lightblue' }} 
-        onClick={handleBackClick}>
+
+      <Button
+        variant="contained"
+        style={{ backgroundColor: "lightblue" }}
+        onClick={handleBackClick}
+      >
         Back
       </Button>
-      <Q_Delete token={token} selectedQuestionID={selectedQuestion.id} author={selectedQuestion.author}/>
+
+      <Q_Delete
+        token={token}
+        selectedQuestionID={selectedQuestion.id}
+        author={selectedQuestion.author}
+      />
+      <Q_Edit token={token} selectedQuestionID={selectedQuestion.id} />
     </>
   );
 };
