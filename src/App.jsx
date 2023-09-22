c33f4e8753884585426da859ce93058b53c831c9
Nathan Vogt <merrick.vogt@gmail.com>
2023-09-22 11:24:33 -0400

    added fetch request and new route for  q_detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,25 @@
-import { useState, useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import useLocalStorageState from 'use-local-storage-state';
-import NavBar from 'components/NavBar';
-import Login from 'components/Login';
-import Register from 'components/Register';
-import Q_Detail from 'components/Q_Detail';
-import Q_Feed from 'components/Q_Feed';
-import Q_Create from './components/Q_Create';
-import Q_Delete from './components/Q_Delete';
-import User_Profile from './components/User_Profile';
-import Footer from './components/Footer';
-import axios from 'axios';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import useLocalStorageState from "use-local-storage-state";
+import NavBar from "components/NavBar";
+import Login from "components/Login";
+import Register from "components/Register";
+import Q_Detail from "components/Q_Detail";
+import Q_Feed from "components/Q_Feed";
+import User_Profile from "./components/User_Profile";
+import Footer from "./components/Footer";
+import axios from "axios";
+import "./App.css";
 // import { fetchData } from './assets/requests';
 
 function App() {
-  const [token, setToken] = useLocalStorageState('userToken', '');
+  const [token, setToken] = useLocalStorageState("userToken", "");
   const [isLoggedIn, setIsLoggedIn] = useLocalStorageState(
-    'userIsLoggedIn',
+    "userIsLoggedIn",
     false
   );
   const [searchResults, setSearchResults] = useState(null);
   const [selectedQuestion, setSelectedQuestion] = useState(null);
-
-  const handleQuestionClick = (question) => {
-    setSelectedQuestion(question);
-  };
 
   useEffect(() => {
     // Define an async function
@@ -34,7 +28,7 @@ function App() {
         // Make the API call
         const response = await axios.get(`https://qb.fly.dev/questions`, {
           headers: {
-            Accept: 'application/json',
+            Accept: "application/json",
             Authorization: `Token ${token}`,
           },
         });
@@ -43,7 +37,7 @@ function App() {
         setSearchResults(response.data);
       } catch (error) {
         // Handle the error
-        console.error('There was an error fetching data', error);
+        console.error("There was an error fetching data", error);
       }
     };
 
@@ -59,7 +53,7 @@ function App() {
       />
       <Routes>
         <Route
-          path='/'
+          path="/"
           element={
             <>
               <Q_Feed
@@ -67,25 +61,31 @@ function App() {
                 searchResults={searchResults}
                 setSelectedQuestion={setSelectedQuestion}
               />
-              <Q_Detail
-                token={token}
-                selectedQuestion={selectedQuestion}
-                setSelectedQuestion={setSelectedQuestion}
-              />
             </>
           }
         />
         <Route
-          path='/profile'
+          path="/questions/:questionID"
+          element={
+            <Q_Detail
+              token={token}
+              selectedQuestion={selectedQuestion}
+              setSelectedQuestion={setSelectedQuestion}
+            />
+          }
+        />
+
+        <Route
+          path="/profile"
           element={<User_Profile token={token} isLoggedIn={isLoggedIn} />}
         />
 
         <Route
-          path='/login'
+          path="/login"
           element={<Login setToken={setToken} setIsLoggedIn={setIsLoggedIn} />}
         />
         <Route
-          path='/register'
+          path="/register"
           element={
             <Register setToken={setToken} setIsLoggedIn={setIsLoggedIn} />
           }
