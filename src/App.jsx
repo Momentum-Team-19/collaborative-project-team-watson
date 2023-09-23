b8d8f6af674cc15773c4df0eb227947e3d9473a6
davis <davisfpatterson@gmail.com>
2023-09-23 12:56:04 -0400

    un-commented API fetch stuff

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-
-import { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
-import useLocalStorageState from "use-local-storage-state";
-import NavBar from "components/NavBar";
-import Login from "components/Login";
-import Register from "components/Register";
-import Q_Detail from "components/Q_Detail";
-import Q_Feed from "components/Q_Feed";
-import User_Profile from "./components/User_Profile";
-import Footer from "./components/Footer";
-import axios from "axios";
-import "./App.css";
-import questions from "./components/questions";
+import { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import useLocalStorageState from 'use-local-storage-state';
+import NavBar from 'components/NavBar';
+import Login from 'components/Login';
+import Register from 'components/Register';
+import Q_Detail from 'components/Q_Detail';
+import Q_Feed from 'components/Q_Feed';
+import User_Profile from './components/User_Profile';
+import Footer from './components/Footer';
+import axios from 'axios';
+import './App.css';
+// import questions from "./components/questions";
 import User_Edit from './components/User_Edit';
 // import { fetchData } from './assets/requests';
 
@@ -21,34 +20,33 @@ function App() {
     'userIsLoggedIn',
     false
   );
-  const [searchResults, setSearchResults] = useState(questions);
+  const [searchResults, setSearchResults] = useState([]);
   const [selectedQuestion, setSelectedQuestion] = useState(null);
   const [searchInput, setSearchInput] = useState('');
 
   // Commented out to avoid API requests with fly.dev.io down
-  // useEffect(() => {
-  //   // Define an async function
-  //   const fetchSearchResults = async () => {
-  //     try {
-  //       // Make the API call
-  //       const response = await axios.get(`https://qb.fly.dev/questions`, {
-  //         headers: {
-  //           Accept: 'application/json',
-  //           Authorization: `Token ${token}`,
-  //         },
-  //       });
+  useEffect(() => {
+    // Define an async function
+    const fetchSearchResults = async () => {
+      try {
+        // Make the API call
+        const response = await axios.get(`https://qb.fly.dev/questions`, {
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Token ${token}`,
+          },
+        });
 
-  //       // Set the response data to state
-  //       setSearchResults(response.data);
-  //     } catch (error) {
-  //       // Handle the error
-  //       console.error("There was an error fetching data", error);
-  //     }
-  //   };
+        // Set the response data to state
+        setSearchResults(response.data);
+      } catch (error) {
+        // Handle the error
+        console.error('There was an error fetching data', error);
+      }
+    };
 
-
-  //   fetchSearchResults();
-  // }, [token]);
+    fetchSearchResults();
+  }, [token]);
 
   return (
     <>
