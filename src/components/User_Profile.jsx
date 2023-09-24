3d0b505dd36b56eea3afe6077abceb491e930ae9
davis <davisfpatterson@gmail.com>
2023-09-24 00:29:18 -0400

    updated user profile with info and their questions

--- src/components/User_Profile.jsx
+++ src/components/User_Profile.jsx
@@ -1,49 +1,85 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import Q_Box from 'components/Q_Box';
 
 const User_Profile = ({ token, isLoggedIn }) => {
   const [userInfo, setUserInfo] = useState(null);
+  const [questionInfo, setQuestionInfo] = useState([]);
 
   useEffect(() => {
-    const fetchUserInfo = async () => {
-      if (isLoggedIn) {
-        try {
-          const response = await axios.get(
-            `https://qb.fly.dev/auth/users/me/`,
-            {
+    if (isLoggedIn) {
+      console.log(isLoggedIn);
+      const fetchUserInfo = async () => {
+        if (isLoggedIn) {
+          try {
+            const userInfoUrl = `https://qb.fly.dev/auth/users/me/`;
+            const userInfoResponse = await axios.get(userInfoUrl, {
               headers: {
                 Accept: 'application/json',
                 Authorization: `Token ${token}`,
               },
-            }
-          );
-          console.log(userInfo);
-
-          setUserInfo(response.data);
-        } catch (error) {
-          console.error('There was an error fetching data', error);
+            });
+            setUserInfo(userInfoResponse.data);
+            const questionInfoUrl = 'https://qb.fly.dev/questions/me';
+            const questionInfoResponse = await axios.get(questionInfoUrl, {
+              headers: {
+                Accept: 'application/json',
+                Authorization: `Token ${token}`,
+              },
+            });
+            setQuestionInfo(questionInfoResponse.data);
+          } catch (error) {
+            console.error('There was an error fetching data', error);
+          }
+        } else {
+          setUserInfo(null);
         }
-      } else {
-        setUserInfo(null);
-      }
-    };
+      };
 
-    console.log(isLoggedIn);
+      console.log(isLoggedIn);
 
-    fetchUserInfo();
+      fetchUserInfo();
+    }
   }, [token, isLoggedIn]);
 
-  return (
-    <>
-      <h2>User Profile</h2>
-      {userInfo ? <h3>{userInfo.username}</h3> : null}
-      {userInfo ? <h4>{userInfo.email}</h4> : null}
-      <Link to={{ pathname: '/profile/edit' }} className='signUpText'>
-        EDIT
-      </Link>
-    </>
-  );
+  if (userInfo) {
+    return (
+      <>
+        <div className='profileContainer'>
+          <div className='profileInfoContainer'>
+            <h2 className='profileInfo'>Username: {userInfo.username}</h2>
+            <h4 className='profileInfo'>
+              Name: {userInfo.first_name || 'N/A'} {userInfo.last_name}
+            </h4>
+            <h5 className='profileInfo'>Phone: {userInfo.phone || 'N/A'}</h5>
+            <h5 className='profileInfo'>Email: {userInfo.email || 'N/A'}</h5>
+          </div>
+          <Link to={{ pathname: '/profile/edit' }} className='editButton'>
+            EDIT
+          </Link>
+        </div>
+        <div className='userQuestionsContainer'>
+          <p className='recentActivityText'>Questions:</p>
+          <div className='qBoxes'>
+            {questionInfo &&
+              questionInfo.map((question) => (
+                <Link to={`/questions/${question.id}`} key={question.id}>
+                  <Q_Box question={question} />
+                </Link>
+              ))}
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h2>User Profile</h2>
+        <h4>No User Data Found</h4>
+      </>
+    );
+  }
 };
 
 export default User_Profile;
