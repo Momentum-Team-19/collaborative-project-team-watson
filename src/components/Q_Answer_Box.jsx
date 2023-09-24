cc84ca2f4c7d11fc15ba91f81798b484faaf9c73
Nathan Vogt <merrick.vogt@gmail.com>
2023-09-24 17:36:47 -0400

    conditional rendering for answer_accept but not updating backend

--- src/components/Q_Answer_Box.jsx
+++ src/components/Q_Answer_Box.jsx
@@ -1,54 +1,53 @@
-import React, { useEffect, useState } from 'react';  // Added useState
-import axios from 'axios';  // Assuming you're using axios
-import { Card, CardContent, Typography } from '@mui/material';
-import Q_Answer_Accepted from './Q_Answer_Accepted';
+import React, { useEffect, useState } from "react"; // Added useState
+import axios from "axios"; // Assuming you're using axios
+import { Card, CardContent, Typography } from "@mui/material";
+import Q_Answer_Accepted from "./Q_Answer_Accepted";
 
-const Q_Answer_Box = ({ answer, token }) => {  // Assuming token is passed as prop
+const Q_Answer_Box = ({ answer, token, loggedInUser }) => {
+  // Assuming token is passed as prop
   const [currentAnswer, setCurrentAnswer] = useState(answer);
-  
 
   const handleAnswerAccepted = (updatedAnswer) => {
     setCurrentAnswer(updatedAnswer);
   };
 
- 
   return (
     <Card
-      className='a-box'
+      className="a-box"
       style={{
-        width: '80%',
-        margin: 'auto',
-        borderRadius: '12px',
-        backgroundColor: answer.accepted ? '#a7e7c3' : '#f2f2f2',
-        marginBottom: '16px',
+        width: "80%",
+        margin: "auto",
+        borderRadius: "12px",
+        backgroundColor: answer.accepted ? "#a7e7c3" : "#f2f2f2",
+        marginBottom: "16px",
       }}
     >
       <CardContent>
         <Typography
-          style={{ fontSize: '14px', fontStyle: 'italic' }}
+          style={{ fontSize: "14px", fontStyle: "italic" }}
           gutterBottom
         >
-          Author: {answer.author || 'Anonymous'}
+          Author: {answer.author || "Anonymous"}
         </Typography>
 
         <Typography
           style={{
-            textOverflow: 'ellipsis',
-            whiteSpace: 'nowrap',
-            overflow: 'hidden',
-            fontSize: '14px',
-            marginBottom: '8px',
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            fontSize: "14px",
+            marginBottom: "8px",
           }}
         >
           Answer: {answer.text}
         </Typography>
 
-        <Q_Answer_Accepted 
+        <Q_Answer_Accepted
           answer={currentAnswer}
           token={token}
           onToggleAccepted={handleAnswerAccepted}
+          loggedInUser={loggedInUser}
         />
-
       </CardContent>
     </Card>
   );
