fc78cb357edad547370a7fe309fcf052845d06cd
Nathan Vogt <merrick.vogt@gmail.com>
2023-09-26 09:08:05 -0400

    answer accept working

--- src/components/Q_Answer_Accepted.jsx
+++ src/components/Q_Answer_Accepted.jsx
@@ -3,12 +3,12 @@ import axios from "axios";
 
 const Q_Answer_Accepted = ({ token, answer, loggedInUser, onToggleAccepted }) => {
     const [questionData, setQuestionData] = useState(null);
-    console.log('answer accepted', answer.accepted)
+    
 
     const handleAnswerAccepted = async (e) => {
         
         const updatedAcceptedState = !answer.accepted;  // Toggle the current state
-        console.log('updatedAcceptedState', updatedAcceptedState)
+        
         try {
             const response = await axios.patch(
                 `https://qb.fly.dev/answers/${answer.id}/accept`,
@@ -20,9 +20,6 @@ const Q_Answer_Accepted = ({ token, answer, loggedInUser, onToggleAccepted }) =>
                     },
                 }
             );
-            console.log('response', response.data)
-            console.log
-
             
             onToggleAccepted(updatedAcceptedState);
             
